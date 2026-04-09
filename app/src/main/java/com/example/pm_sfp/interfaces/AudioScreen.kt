9f5a8af5d857238a9545ae6ddafa266d0e32ac45
ajcarmona99 <carmonaantoniojesus@gmail.com>
2026-04-09 21:04:45 +0200

    Sesion5

--- app/src/main/java/com/example/pm_sfp/interfaces/AudioScreen.kt
+++ app/src/main/java/com/example/pm_sfp/interfaces/AudioScreen.kt
@@ -1,9 +1,12 @@
 package com.example.pm_sfp.interfaces
 
-
-import android.R.attr.onClick
-import android.inputmethodservice.Keyboard
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,54 +15,83 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import com.example.pm_sfp.media.SimpleAudioPlayer
-import com.example.pm_sfp.storage.AppFiles
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-
+import com.example.pm_sfp.media.SimpleAudioPlayer
+import com.example.pm_sfp.media.SimpleAudioRecorder
+import com.example.pm_sfp.storage.AppFiles
 
 @Composable
 fun AudioScreen(){
+    val context = LocalContext.current
+    val audioFile = remember { AppFiles.audioFile(context) }
 
-    var context = LocalContext.current
-    var audioFile = remember { AppFiles.audioFile(context) }
+    val (hasAudioPerm, requestAudioPerm) = rememberAudioPermission()
+
     var status by remember { mutableStateOf("Listo") }
-    var player = remember { SimpleAudioPlayer() }
+
+    val player = remember { SimpleAudioPlayer() }
+    val recoder = remember { SimpleAudioRecorder() }
 
     DisposableEffect(Unit) {
         onDispose {
+            recoder.stop()
             player.release()
         }
     }
 
-    Column(horizontalAlignment = Arrangement.spacedBy(8.dp) as Alignment.Horizontal) {
-        Button(onClick = {
-            status = "Preparando..."
-            player.prepareFromFile(
-                file = audioFile,
-                onCompleted = {status = "Terminado"},
-                onError = {msg -> status = msg}
-            )
-        }) { Text("Preparar")}
-        Button(onClick = {
-            player.play { status = it }
-            if (status == "Preparado") status = "Reproduciendo..."
-        }) {Text("Play") }
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Text("Audio - Grabar y reproducir")
+        Text("Permiso microfono: ${if (hasAudioPerm) "OK" else "NO"}")
+        Text("Archivo: ${audioFile.name} (${if (audioFile.exists()) "existe" else "no existe"})")
+        Text("Estado : {$status}")
 
-        Button(
-            onClick = {
-                player.pause()
-                status = "Pausado"
+        if(!hasAudioPerm){
+            Button(onClick = requestAudioPerm) { Text("Pedir permiso de microfono") }
+        }else{
+            Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                Button(onClick = {
+                    player.stop()
+                    recoder.start(audioFile){status = it}
+                    status = "Grabando ..."
+                },
+                    enabled = !recoder.isRecording()) {
+                    Text("REC")
+                }
+
+                Button(
+                    onClick = {
+                        recoder.stop()
+                        status = "Grabación guardada"
+                    }, enabled = recoder.isRecording()
+                ) { Text("STOP REC")}
             }
-        ) {Text("Pausado") }
 
-        Button(onClick = {
-            player.stop()
-            status = "Parado"
-        }) { Text("Stop") }
+            Spacer(Modifier.height(8.dp))
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Button(onClick = {
+                    status = "Preparando..."
+                    player.prepareFromFile(
+                        file = audioFile,
+                        onCompleted = {status = "Terminado"},
+                        onError = {status = it}
+                    )
+                }) {Text("Preparar") }
+                Button(onClick = {
+                    player.play { status = it }
+                    if (status == "Preparado") status = "Reproduciendo..."
+                }) {Text("Play") }
+                Button(onClick = {player.pause(); status = "Pausado"}) {Text(("Pause")) }
+                Button(onClick = {player.stop(); status = "Parado"}) {Text("Stop") }
+            }
+        }
     }
-
 
 }
