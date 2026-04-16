0c769f436624589e5a818f37934159659eb1cea6
ajcarmona99 <carmonaantoniojesus@gmail.com>
2026-04-16 20:56:05 +0200

    Sesion6

--- app/src/main/java/com/example/pm_sfp/MainActivity.kt
+++ app/src/main/java/com/example/pm_sfp/MainActivity.kt
@@ -4,12 +4,13 @@ import com.example.pm_sfp.interfaces.AudioScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.pm_sfp.interfaces.CameraScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                AudioScreen()
+            CameraScreen()
             }
         }
     }
